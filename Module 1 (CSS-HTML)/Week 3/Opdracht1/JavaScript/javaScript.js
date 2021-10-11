9e42e3b21ccdc95837b6865575b1ca6747098c6f
QuillanW <56843492+QuillanW@users.noreply.github.com>
2021-10-11 13:48:32 +0200

    Add files via upload

---
+++ Module 1 (CSS-HTML)/Week 3/Opdracht1/JavaScript/javaScript.js
@@ -0,0 +1,11 @@
+function checkpassword() {
+    var password = document.getElementById("passwordBox");
+    var passwordText = password.value;
+    if (passwordText == "code") {
+        return true;
+    }
+    alert("incorrect password")
+    return false;
+    var data = document.getElementById("data1")
+    return console.log(data)
+}
